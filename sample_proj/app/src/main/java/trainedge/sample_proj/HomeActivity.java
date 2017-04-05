b7e5d50f291e744c45cebc9aabf080e56ece1e42
Ankursng <singhankur275@gmail.com>
2017-04-05 11:08:31 +0530

    updated menu
    haha

--- sample_proj/app/src/main/java/trainedge/sample_proj/HomeActivity.java
+++ sample_proj/app/src/main/java/trainedge/sample_proj/HomeActivity.java
@@ -80,9 +80,10 @@ public class HomeActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_task) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+            
+        } else if (id == R.id.nav_view) {
 
         } else if (id == R.id.nav_slideshow) {
 
