1a910633aced4cd480aa314a7f9e5897308bed04
Abhishek Verma <abhi.s4422@gmail.com>
2017-04-18 18:37:52 +0530

    feeedback code
    hahaha

--- sample_proj/app/src/main/java/trainedge/sample_proj/FeedbackActivity.java
+++ sample_proj/app/src/main/java/trainedge/sample_proj/FeedbackActivity.java
@@ -5,15 +5,70 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
-public class FeedbackActivity extends AppCompatActivity {
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import java.util.HashMap;
+
+public class FeedbackActivity extends AppCompatActivity {
+    Button b;
+    EditText e1,e2;
+    CheckBox c;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+        b=(Button)findViewById(R.id.button);
+        e1=(EditText)findViewById(R.id.editText1);
+        e2= (EditText) findViewById(R.id.editText2);
+        c= (CheckBox) findViewById(R.id.checkBox);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_SEND);
+                i.setType("message/html");
+                i.putExtra(Intent.EXTRA_EMAIL, new String[]{"abhi.s4422@gmail.com"});
+                i.putExtra(Intent.EXTRA_SUBJECT, "Feedback from App");
+                i.putExtra(Intent.EXTRA_TEXT, "Email : "+e1.getText()+"\nMessage : "+e2.getText()+"\nCheckbox"+c.isChecked());
+                try {
+                    startActivity(Intent.createChooser(i, "Send feedback..."));
+                } catch (android.content.ActivityNotFoundException ex) {
+                    Toast.makeText(getApplicationContext(), "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        // getMenuInflater().inflate(R.menu.menu_main, menu);
+        return false;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
