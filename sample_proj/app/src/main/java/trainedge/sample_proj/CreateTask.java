4707c7b173878529893ebc28c2844e85caeb590c
Ankursng <singhankur275@gmail.com>
2017-04-25 12:00:46 +0530

    new create page
    need help in date picker

--- sample_proj/app/src/main/java/trainedge/sample_proj/CreateTask.java
+++ sample_proj/app/src/main/java/trainedge/sample_proj/CreateTask.java
@@ -8,8 +8,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class CreateTask extends AppCompatActivity {
+
+    private TextView txtv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +20,16 @@ public class CreateTask extends AppCompatActivity {
         setContentView(R.layout.activity_create_task);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        Button button = (Button) findViewById(R.id.button2);
-        button.setOnClickListener(new View.OnClickListener() {
+
+        txtv = (TextView) findViewById(R.id.txtv);
+        txtv.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent(CreateTask.this, PlaceSelectionActivity.class);
                 startActivity(intent);
             }
         });
+
+
 
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
